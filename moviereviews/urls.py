32f063c5004f0a476a567130413df2a43e71402c
Samuel Daza <sdazac@eafit.edu.co>
2025-07-31 11:48:46 -0500

    busqueda de peliculas

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -18,8 +18,15 @@ from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', movieViews.home),
     path('about/', movieViews.about),  # 👈 Nueva ruta
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
